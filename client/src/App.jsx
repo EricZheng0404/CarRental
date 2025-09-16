6dd2dab84f60f0ac4646a395e6ec2d3c83876716
Eric Zheng <erryzheng@gmail.com>
2025-09-16 00:58:34 -0400

    added hero on page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import CarDetails from './pages/CarDetails'
+import MyBookings from './pages/MyBookings'
+import Cars from './pages/Cars'
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false)
@@ -14,6 +16,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/car-details/:id' element={<CarDetails/>} />
+        <Route path='/my-bookings' element={<MyBookings/>} />
+        <Route path='/cars' element={<Cars/>} />
       </Routes>
     </>
   )
