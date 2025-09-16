022edcead6eb2537d9d33b4d6c59d5dcf39d41c7
Eric Zheng <erryzheng@gmail.com>
2025-09-16 00:41:25 -0400

    new names

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import CarDetails from './pages/CarDetails'
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false)
@@ -8,6 +10,11 @@ const App = () => {
   return (
     <>
       {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}      
+
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/car-details/:id' element={<CarDetails/>} />
+      </Routes>
     </>
   )
 }
